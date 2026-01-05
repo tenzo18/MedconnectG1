c5dfb52dd2d413175b9729d6c103d54b08ab3556
tenzo <=>
2026-01-06 00:14:10 +0100

    Update API URL to HTTPS domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: (typeof window !== 'undefined' && (window as any).__API_URL__) 
     ? (window as any).__API_URL__ 
-    : 'http://194.238.25.170:5000/api'
+    : 'https://medconnectapi.lafaom-mao.org/api'
 };
