b0724df8f6e0ff7b196177d370927a67d18eecf2
tenzo <=>
2026-01-06 00:05:27 +0100

    Fix TypeScript errors in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,5 @@ export const environment = {
   production: true,
   apiUrl: (typeof window !== 'undefined' && (window as any).__API_URL__) 
     ? (window as any).__API_URL__ 
-    : (typeof process !== 'undefined' && process.env?.['API_URL']) 
-    ? process.env['API_URL']
     : 'http://194.238.25.170:5000/api'
 };
